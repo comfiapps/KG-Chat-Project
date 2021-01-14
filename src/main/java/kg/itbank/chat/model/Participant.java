156530a63e980429224736aba68c3ce7232b30b8
ScaleUP <comfiapps@gmail.com>
2021-01-14 11:12:11 +0900

    participant update

--- src/main/java/kg/itbank/chat/model/Participant.java
+++ src/main/java/kg/itbank/chat/model/Participant.java
@@ -7,13 +7,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Builder
-public class Participant {
+public class Participant implements Serializable {
 
     @Id
     @ManyToOne
