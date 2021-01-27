1bfb8f22b4d31cd3f70ebc9a88279545a55c4905
ScaleUP <comfiapps@gmail.com>
2021-01-27 20:46:17 +0900

    pom edit

--- src/main/java/kg/itbank/chat/ChatApplication.java
+++ src/main/java/kg/itbank/chat/ChatApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatApplication extends SpringBootServletInitializer {
+public class ChatApplication {
 
     public static int DEBATE_TIME = 30;
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ChatApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
